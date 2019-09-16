0dd2c2d7c0e31dc26fc7aed87ddec617e0c09c95
Phonthep Aungkanukulwit <mos11134@gmail.com>
2019-09-16 16:35:15 +0700

    [INIT] initial project

---
+++ MobileBuyerGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileBuyerGuide
+//
+//  Created by Sakarn Limnitikarn on 31/7/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+use_frameworks!
+
+target 'MobileBuyerGuide' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MobileBuyerGuide
+pod 'Alamofire', '~> 5.0.0-beta.5'
+pod 'SwiftyJSON', '~> 5.0'
+end
